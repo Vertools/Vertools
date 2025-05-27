44d90c156e98591e3b8625f5d0c5e2981e44d054
Vertools <fireflou1@gmail.com>
2025-05-27 22:39:43 +0200

    server.js

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const fs = require("fs");
 
-app.use(express.static(__dirname)); // sert index.html et les fichiers
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 
 let users = {};
+let messages = {};
+
+if (fs.existsSync("messages.json")) {
+  messages = JSON.parse(fs.readFileSync("messages.json"));
+}
+
+function saveMessages() {
+  fs.writeFileSync("messages.json", JSON.stringify(messages));
+}
 
 io.on("connection", socket => {
   socket.on("login", pseudo => {
@@ -14,8 +25,23 @@ io.on("connection", socket => {
     io.emit("users", Object.values(users));
   });
 
-  socket.on("message", message => {
-    io.emit("message", { pseudo: socket.pseudo, message });
+  socket.on("privateMessage", ({ to, message }) => {
+    const key = [socket.pseudo, to].sort().join("-");
+    if (!messages[key]) messages[key] = [];
+    messages[key].push({ from: socket.pseudo, message });
+    saveMessages();
+
+    for (let id in users) {
+      if (users[id] === to) {
+        io.to(id).emit("privateMessage", { from: socket.pseudo, message });
+      }
+    }
+  });
+
+  socket.on("loadMessages", (withUser) => {
+    const key = [socket.pseudo, withUser].sort().join("-");
+    const chat = messages[key] || [];
+    socket.emit("loadMessages", chat);
   });
 
   socket.on("disconnect", () => {
@@ -24,6 +50,4 @@ io.on("connection", socket => {
   });
 });
 
-http.listen(3000, () => {
-  console.log("Serveur lancé sur http://localhost:3000");
-});
+http.listen(3000, () => console.log("Serveur sur http://localhost:3000"));
