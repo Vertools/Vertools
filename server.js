4d1e74db798d8436f037ac2203eb30465997bf33
Vertools <fireflou1@gmail.com>
2025-05-28 10:49:03 +0200

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const fs = require("fs");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const multer = require("multer");
 const path = require("path");
 
